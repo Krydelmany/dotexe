edcf6c61314c1c6e7605681e0be0300a46fdf58a
Giovani Claro Moraes <giclamoraes@gmail.com>
2024-09-26 11:34:19 -0300

    Remoção dos arquivos e alterados para as respectivas pastas.

--- excluir-jogo.h
+++
@@ -1,7 +0,0 @@
-#ifndef EXCLUIR_JOGO_H
-#define EXCLUIR_JOGO_H
-
-// Declaração da função que exclui um jogo com base no ID
-void excluirJogo(int id);
-
-#endif
--- gravar-jogo.h
+++
@@ -1,8 +0,0 @@
-#ifndef GRAVAR_JOGO_H
-#define GRAVAR_JOGO_H
-
-#include <string>
-
-void gravarJogo(int id, const std::string& nomeJogo, const std::string& generoJogo, const std::string& plataforma, const std::string& anoLancamento, const std::string& desenvolvedor);
-
-#endif
