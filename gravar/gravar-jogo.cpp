977f8ccc16a14c4d08683470a29d3ff999fa7b49
Giovani Claro Moraes <giclamoraes@gmail.com>
2024-09-30 03:23:13 -0300

    Refatorar a função gravarJogo e atualizar a documentação

--- gravar/gravar-jogo.cpp
+++ gravar/gravar-jogo.cpp
@@ -1,20 +1,39 @@
-#include "gravar-jogo.h"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "gravar-jogo.h"
 
-void gravarJogo(int id, const std::string& nomeJogo, const std::string& generoJogo, const std::string& plataforma, const std::string& anoLancamento, const std::string& desenvolvedor) {
-    std::ofstream arquivo("output/jogos.txt", std::ios::app);
+/**
+ * @brief Função para gravar um jogo no arquivo.
+ * 
+ * Esta função abre um arquivo contendo uma lista de jogos e adiciona um novo jogo
+ * com os detalhes fornecidos. O arquivo é aberto em modo de anexação para garantir
+ * que o novo jogo seja adicionado ao final do arquivo existente.
+ * 
+ * @param id O ID do jogo.
+ * @param nome O nome do jogo.
+ * @param genero O gênero do jogo.
+ * @param plataforma A plataforma do jogo.
+ * @param anoLancamento O ano de lançamento do jogo.
+ * @param desenvolvedor O desenvolvedor do jogo.
+ */
+void gravarJogo(int id, const std::string& nome, const std::string& genero, 
+                const std::string& plataforma, const std::string& anoLancamento, 
+                const std::string& desenvolvedor) {
+    std::ofstream arquivo("output/jogos.txt", std::ios::app); // Abre o arquivo em modo de anexação
+
     if (arquivo.is_open()) {
-        arquivo << id << "; Nome: " << nomeJogo 
-                << ", Genero: " << generoJogo 
-                << ", Plataforma: " << plataforma 
-                << ", Ano: " << anoLancamento 
-                << ", Desenvolvedor: " << desenvolvedor << ";"
-                << std::endl;
+            
+        arquivo << "ID: " << id << "; "
+                << "Nome: " << nome << "; "
+                << "Genero: " << genero << "; "
+                << "Plataforma: " << plataforma << "; "
+                << "Ano: " << anoLancamento << "; "
+                << "Desenvolvedor: " << desenvolvedor << "\n";
+
         arquivo.close();
         std::cout << "Jogo gravado com sucesso!" << std::endl;
     } else {
-        std::cerr << "Erro ao abrir o arquivo!" << std::endl;
+        std::cerr << "Erro ao abrir o arquivo para gravar o jogo." << std::endl;
     }
 }
